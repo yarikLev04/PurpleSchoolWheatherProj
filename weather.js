c9143cbb965eb0825ba44217ce9745f00a90450e
yarikLev04 <91241935+yarikLev04@users.noreply.github.com>
2023-03-27 15:12:35 +0300

    ended logic

--- weather.js
+++ weather.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 import { getArgs } from './helpers/args.js';
-import { printError, printHelp, printSuccess } from './services/log.js';
-import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.js';
-import { getWeather } from './services/api.js';
+import { printError, printHelp, printSuccess, printWeather } from './services/log.js';
+import { getKeyValue, saveKeyValue, TOKEN_DICTIONARY } from './services/storage.js';
+import { getIcon, getWeather } from './services/api.js';
 
 const saveToken = async (token) => {
 	if (!token.length) {
@@ -18,18 +18,48 @@ const saveToken = async (token) => {
 	}
 }
 
+const saveCity = async (city) => {
+	if (!city.length) {
+		printError('Не передан город')
+		return
+	}
+	try {
+		await saveKeyValue(TOKEN_DICTIONARY.city, city)
+		printSuccess('Город сохранен')
+	} catch (e) {
+		printError(e.message)
+	}
+}
+
+const getForcast  = async () => {
+	try	{
+		const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city);
+		const weather = await getWeather(city);
+		printWeather(weather, getIcon(weather.weather[0].icon))
+	} catch (e) {
+		if (e?.response?.status === 404) {
+			printError('Неверно указан город')
+		} else if (e?.response?.status === 401) {
+			printError('Неверно указан токен')
+		} else {
+			printError(e.message);
+		}
+	}
+}
+
 const initCLI = () => {
 	const args = getArgs(process.argv);
+	// console.log(process.env);
 	if (args.h) {
-		printHelp();
+		return printHelp();
 	}
 	if (args.s) {
-
+		return saveCity(args.s)
 	}
 	if (args.t) {
 		return saveToken(args.t)
 	}
-	getWeather('London')
+	return getForcast()
 };
 
 initCLI();
